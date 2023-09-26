d0148dcb9262600bec4b2fb1cea0240bde976b61
Laith banat <laithahmad201092@gmail.com>
2023-09-27 00:55:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = 'John';
+
+// Define the welcomeMessage function
+function welcomeMessage(currentUser) {
+  return `Welcome to Flatbook, ${currentUser}!`;
+}
+
+// Define the excitedWelcomeMessage function
+function excitedWelcomeMessage(currentUser) {
+  return `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+}
+
+// Define the shortGreeting function
+function shortGreeting(currentUser) {
+  return `Welcome, ${currentUser.charAt(0)}!`;
+}
+
+module.exports = { currentUser, welcomeMessage, excitedWelcomeMessage, shortGreeting };
